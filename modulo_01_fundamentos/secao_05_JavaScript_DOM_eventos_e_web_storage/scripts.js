702580bf58ea7bd864ba2c2951648a1342af4761
saulomagnus <saulomagnus@outlook.com>
2022-09-29 18:27:19 -0300

    Primeira parte dos exercícios do dia 5.1

---
+++ modulo_01_fundamentos/secao_05_JavaScript_DOM_eventos_e_web_storage/scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink';
+
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+
+document.getElementById('footer-container').style.backgroundColor = 'blue';
